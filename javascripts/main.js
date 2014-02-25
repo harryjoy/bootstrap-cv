610bbfa60e6347c51b4e8a1ca432936fb231d83a
Harsh Raval <harshraval9@gmail.com>
2014-02-26 00:26:23 +0530

    Updated index page to be the demo page.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,53 +1,75 @@
-var sectionHeight = function() {
-  var total    = $(window).height(),
-      $section = $('section').css('height','auto');
-
-  if ($section.outerHeight(true) < total) {
-    var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 20);
-  } else {
-    $section.css('height','auto');
-  }
-}
-
-$(window).resize(sectionHeight);
-
-$(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
-  });
-  
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();    
-  });
-  
-  sectionHeight();
-  
-  $('img').load(sectionHeight);
+$('html').localScroll();
+$('body').scrollspy({ target: '.navbar-default' })
+$('span.grade').tooltip()
+$(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
 });
-
-fixScale = function(doc) {
-
-  var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
-
-  function fix() {
-    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-    doc.removeEventListener(type, fix, true);
-  }
-
-  if ((meta = meta[meta.length - 1]) && addEvent in doc) {
-    fix();
-    scales = [.25, 1.6];
-    doc[addEvent](type, fix, true);
-  }
+$("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 1000);
+});
+var opts = {
+  lines: 12,
+  angle: 0.5,
+  lineWidth: 0.08,
+  pointer: {
+    length: 0.71,
+    strokeWidth: 0.024,
+    color: '#000000'
+  },
+  limitMax: 'false',
+  colorStart: '#DFEB44',
+  colorStop: '#D0E040',
+  strokeColor: '#E0E0E0',
+  generateGradient: true
 };
+var target = document.getElementById('website-circle');
+var gauge = new Donut(target).setOptions(opts);
+gauge.maxValue = 100;
+gauge.animationSpeed = 64;
+gauge.set(90);
+
+opts = {
+	lines: 12,
+	angle: 0.5,
+	lineWidth: 0.08,
+	pointer: {
+		length: 0.71,
+		strokeWidth: 0.024,
+		color: '#000000'
+	},
+	limitMax: 'false',
+	colorStart: '#2AC96A',
+	colorStop: '#20C06A',
+	strokeColor: '#E0E0E0',
+	generateGradient: true
+};
+target = document.getElementById('photoshop-circle');
+gauge = new Donut(target).setOptions(opts);
+gauge.maxValue = 100;
+gauge.animationSpeed = 64;
+gauge.set(80);
+
+opts = {
+	lines: 12,
+	angle: 0.5,
+	lineWidth: 0.08,
+	pointer: {
+		length: 0.71,
+		strokeWidth: 0.024,
+		color: '#000000'
+	},
+	limitMax: 'false',
+	colorStart: '#10A04E',
+	colorStop: '#8FC0DA',
+	strokeColor: '#E0E0E0',
+	generateGradient: true
+};
+target = document.getElementById('other-circle');
+gauge = new Donut(target).setOptions(opts);
+gauge.maxValue = 100;
+gauge.animationSpeed = 64;
+gauge.set(85);
